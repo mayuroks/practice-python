d949d780d27d149a28a13f6fabd9ef3f2759fc8b
mayuroks <mayuroks@gmail.com>
2013-07-05 03:21:04 +0530

    ayaya

---
+++ pyv2/flask-web2py/web2py/applications/welcome/modules/sim.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf8
+from gluon import *
+#!/usr/bin/env python
+# coding: utf8
+from gluon import *
+# coding: utf8
+# try something like
+import pylast, re
+def similar_search(artist, song):
+  '''USAGE : similar_search('rihanna', 'umbrella')
+  OUTPUT is a LIST passed to thread-pool manager
+  which will decide the rate of download, assign download_music threads etc'''
+  # You have to have your own unique two values for API_KEY and API_SECRET
+  # Obtain yours from http://www.last.fm/api/account for Last.fm
+  API_KEY = "e3d64479d574074eac7058d08de0dda7" # this is a sample key
+  API_SECRET = "063d322566d3a8bcbd48ac160aa5097a"
+
+  # In order to perform a write operation you need to authenticate yourself
+  username = "mayuroks"
+  password_hash = pylast.md5("hydrofuran")
+
+  network = pylast.LastFMNetwork(api_key = API_KEY, api_secret = API_SECRET, username = username, password_hash = password_hash)
+
+  ######### define a track ######
+  #track = network.get_track("rihanna", "umbrella")
+  track = network.get_track(artist, song)
+
+  print(track.get_name)
+
+  ########### MY CODE ###########
+  my_list = track.get_similar()
+  song_list = [(my_list[i][1], str(my_list[i][0])) for i in range(len(my_list))]
+  song_list = sorted(song_list, reverse=True)
+  #print(song_list[:10][8][1])
+  print(song_list[:10])
+  result = []
+  [result.append(i[1]) for i in song_list]
+  print('RESULT SNAP : ', result[:20])
+  return result
+#def index(): return dict(message="hello from sim.py")
+#similar_search("rihanna", "umbrella")
---
+++ pyv2/flask-web2py/web2py/parameters_443.py
@@ -0,0 +1,1 @@
+password="pbkdf2(1000,20,sha512)$bbd7d97d51ecbf78$fba16c5b52bc3170606a07e34a10220cdda3a8f3"
